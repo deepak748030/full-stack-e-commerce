5cc40f2fa00ec3223e5d639ff7cebf0491d166c5
Deepak kushwah <121612353+deepak748030@users.noreply.github.com>
2024-11-26 13:05:37 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -30,15 +30,7 @@ const PORT = process.env.PORT || 5000;
 app.use(
   cors({
     origin: "*",
-    methods: ["GET", "POST", "DELETE", "PUT"],
-    allowedHeaders: [
-      "Content-Type",
-      "Authorization",
-      "Cache-Control",
-      "Expires",
-      "Pragma",
-    ],
-    credentials: true,
+    methods: ["GET", "POST", "DELETE", "PUT"]
   })
 );
 
